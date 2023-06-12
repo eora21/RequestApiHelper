330131f1d6d9d3920f36856f0264fb6ee8e173f2
eora21 <eora21@naver.com>
2023-06-12 16:11:32 +0900

    fix: 🐛 변경 발급받은 토큰이 아닌 인증 유저의 토큰을 이용

--- src/main/java/com/eora21/request_api_helper/service/OAuth2GithubUserService.java
+++ src/main/java/com/eora21/request_api_helper/service/OAuth2GithubUserService.java
@@ -4,14 +4,13 @@ import com.eora21.request_api_helper.model.GatewayUser;
 import com.eora21.request_api_helper.model.GitEmailDto;
 import com.eora21.request_api_helper.util.RequestApiHelper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -19,18 +18,14 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@PropertySource("classpath:application.properties")
 @RequiredArgsConstructor
 public class OAuth2GithubUserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-
-    @Value("${github.token}")
-    private String githubToken;
 
     private final RequestApiHelper requestApiHelper;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        HttpHeaders headers = setHeader();
+        HttpHeaders headers = setHeader(userRequest.getAccessToken());
 
         List<GitEmailDto> response = requestApiHelper.getResponse(
                 "https://api.github.com/user/emails", HttpMethod.GET, headers,
@@ -44,10 +39,10 @@ public class OAuth2GithubUserService implements OAuth2UserService<OAuth2UserRequ
         return new GatewayUser("test", null);
     }
 
-    private HttpHeaders setHeader() {
+    private HttpHeaders setHeader(OAuth2AccessToken accessToken) {
         HttpHeaders headers = requestApiHelper.getDefaultHeaders();
         headers.setAccept(List.of(MediaType.valueOf("application/vnd.github+json")));
-        headers.setBearerAuth(githubToken);
+        headers.setBearerAuth(accessToken.getTokenValue());
         headers.set("X-GitHub-Api-Version", "2022-11-28");
         return headers;
     }
